b93370e1e7a978bd2206311cb9d7bce69ef709bd
Jesús <jesus.lab.tech@gmail.com>
2026-02-12 10:47:39 +0100

    feat: generate coverage and exclude logger

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,13 +2,22 @@
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  // ruta a tu app Next.js
+  // Next.js app route
   dir: './',
 });
 
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+
+  // Generate coverage and exclude logger
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'lib/**/*.ts',
+    '!lib/logger.ts',  // <-- We exclude logger
+  ],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['lcov', 'text'],
 };
 
 module.exports = createJestConfig(customJestConfig);
